302bcdd4fd02e07c5799d604afb3c5506cdebf67
André Vasconcelos <111195307+AndreVsSA@users.noreply.github.com>
2025-04-22 10:28:09 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Custom scripts loaded.");
+});
