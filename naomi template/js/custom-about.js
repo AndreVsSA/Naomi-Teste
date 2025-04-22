8c1e2c67ade89e73662d40061ef2a2768512faa5
André Vasconcelos <111195307+AndreVsSA@users.noreply.github.com>
2025-04-22 10:26:36 -0300

    Delete naomi template directory

--- naomi template/js/custom-about.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("About section loaded successfully.");
-});
--- naomi template/js/custom.js
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Custom scripts loaded.");
-});
